80cf35318c9eb6d738064ad1912574e1c6f098ce
Serafima Avdeeva <s.avdyeyeva.fit.122.20@knute.edu.ua>
2020-10-01 19:44:01 +0300

    files added

--- laboratorna4/data_service.py
+++ laboratorna4/data_service.py
@@ -1,1 +1,13 @@
-print("data service")
+from data.data1 import file1
+from data.data1 import file2
+
+def print1(somelist):
+    """print items in the array"""
+    for t1 in somelist:
+        print (t1)
+
+print('table 1')
+print1(file1)
+
+print('table 2')
+print1(file2)
